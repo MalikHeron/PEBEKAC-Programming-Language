b406976e9e64d15516a76b25149689f718d3eaad
Tyrien Gilpin <91337390+Th3Wol7@users.noreply.github.com>
2024-03-27 13:46:32 -0500

    accepting input

---
+++ generated_code.py
@@ -0,0 +1,31 @@
+def main():
+    graph = None
+    graph = input("What is a graph?")
+    def add(a, c, s):
+        return a + s + c
+    def subtract(d, e, f):
+        return d - e - f
+    def test():
+        print("Testing void return type")
+    test()
+    a = 5
+    c = 6
+    b = 7
+    values = add(a, c, b)
+    a = 10
+    b = 5
+    x = 5
+    code = "John"
+    print("Hello World, my name is ")
+    if x == 10:
+        print("x = 10")
+    else:
+        print("x != 10")
+    while x:
+        print("x is not zero")
+        break
+    print("x = " + str(5))
+    i = 0
+    while i < 10:
+        print("x = " + str(5))
+        i += 1
